42d54f16eba40e90262d780b0c1a0c2df8d96b2b
Aniruddha Sawant <andysawant@gmail.com>
2021-01-06 19:08:02 +1100

    test classes

--- subscription/src/test/java/com/sawant/subscription/SubscriptionApplicationTests.java
+++ subscription/src/test/java/com/sawant/subscription/SubscriptionApplicationTests.java
@@ -1,13 +1,23 @@
 package com.sawant.subscription;
 
+import com.sawant.subscription.model.Subscription;
+import com.sawant.subscription.util.SubscriptionUtil;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @SpringBootTest
 class SubscriptionApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
 
+	}
 }
---
+++ subscription/src/test/java/com/sawant/subscription/SubscriptionUtilTest.java
@@ -0,0 +1,39 @@
+package com.sawant.subscription;
+
+import com.sawant.subscription.model.Subscription;
+import com.sawant.subscription.util.SubscriptionUtil;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class SubscriptionUtilTest {
+    @Autowired
+    SubscriptionUtil subscriptionUtil;
+
+
+    @Test
+    public void testValidSubscription() throws Exception{
+    Subscription sub = new Subscription();
+		sub.setAmount(10);
+		sub.setSubscriptionType("WEEKLY");
+		sub.setDay("TUESDAY");
+		String sDate1="15/02/2021";
+        Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(sDate1);
+		sub.setStartDate(date1);
+        String sDate2="15/02/2021";
+        Date date2=new SimpleDateFormat("dd/MM/yyyy").parse(sDate2);
+		sub.setEndDate(date2);
+    List<String> errorList = subscriptionUtil.validateSubscription(sub);
+
+    assertTrue(errorList.isEmpty());
+    }
+}
